c4d248ddbb311fd9fff3ad2c6bfdad0d8ad97934
Cyril Noirot <cyril.noirot@gmail.com>
2018-06-08 17:16:54 +0100

    all

--- Portfolio/app.py
+++ Portfolio/app.py
@@ -1,13 +1,24 @@
 
 #!/usr/bin/env python3
 
+import sys 
+import os 
+
+## DASH dependencies
+import dash_core_components as dcc 
+import dash_html_components as html 
 import dash 
-#import dash_core_components as dcc 
-#import dash_html_components as html 
 
 
-app = dash.DASH()
+modulename = 'dash'
+if modulename not in sys.modules:
+    print('You have not imported the {} module',format(modulename))
 
+
+
+app = dash.Dash(__name__)
+
+# focus on dataFin 
 app.layout = html.Div('Dashboard')
 
 if __name__ == '__main__': 
